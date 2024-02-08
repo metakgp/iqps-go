e72f2ef30707cc26e61bedf7445c2988668acaca
Rajiv Harlalka <rajivharlalka009@gmail.com>
2024-02-09 01:18:20 +0530

    remove unnecessary code
    Signed-off-by: Rajiv Harlalka <rajivharlalka009@gmail.com>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,7 @@
 import type { Component } from "solid-js";
 
-import logo from "./logo.svg";
-import styles from "./App.module.scss";
-
 const App: Component = () => {
-  return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload. Hello
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
-    </div>
-  );
+  return <div>Hello World</div>;
 };
 
 export default App;
