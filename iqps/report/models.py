15b460593ae238129dab4738745ca1242defd4e5
Chirag Ghosh <cghosh828049@gmail.com>
2024-02-08 23:25:31 +0530

    refactor: rebirth
    Co-authored-by: Rajiv Harlalka <rajivharlalka009@gmail.com>
    Co-authored-by: Shikhar Soni <shikharish05@gmail.com>
    Signed-off-by: Chirag Ghosh <cghosh828049@gmail.com>

--- iqps/report/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-from data.models import Paper
-
-
-class Report(models.Model):
-    paper = models.ForeignKey(Paper, on_delete=models.CASCADE)
-    reason = models.CharField(max_length=1024)
--- sanity.sh
+++
@@ -1,4 +0,0 @@
-dodgy --ignore paths venv env docs migrations
-isort -rc --atomic ./ -sg "*/migrations/*.py" -sg "*/docs/*.py"
-pydocstyle --config=./.pydocstylerc
-pycodestyle ./ --config=./.pycodestylerc
