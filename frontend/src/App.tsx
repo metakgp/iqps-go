d3b4a0a94dac7e6b1b11da50c08467278b358eb3
Harsh Khandeparkar <shadowwarriorpro3003@gmail.com>
2024-02-16 22:51:57 +0530

    feat: added centered title

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,10 @@ import "./styles/styles.scss";
 const App: Component = () => {
   return (
     <div class="hero-screen">
-      <h1>Course Search</h1>
+      <div class="title">
+        <h1>IQPS - Intelligent Question Paper Search</h1>
+        <p><i>Search for question papers when the library can't save you.</i></p>
+      </div>
       <CourseSearchForm />
     </div>
   );
