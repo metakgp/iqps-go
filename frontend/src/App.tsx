c820eec271465bc4e9715f14c92f5bd50ce57d62
Chirag Ghosh <cghosh828049@gmail.com>
2024-02-16 22:03:52 +0530

    feat: i am watching
    Signed-off-by: Chirag Ghosh <cghosh828049@gmail.com>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,14 @@
 import type { Component } from "solid-js";
+import { Analytics } from "@vercel/analytics/react";
 import CourseSearchForm from "./components/searchForm";
 import "./styles/styles.scss";
+
 const App: Component = () => {
   return (
     <div class="hero-screen">
       <h1>Course Search</h1>
       <CourseSearchForm />
+      <Analytics />
     </div>
   );
 };
