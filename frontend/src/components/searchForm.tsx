247dc74f5ae95352435f4c9212b67b7391396360
Harsh Khandeparkar <shadowwarriorpro3003@gmail.com>
2024-02-16 17:42:26 +0530

    feat: removed year, updated exam field

--- frontend/src/components/searchForm.tsx
+++ frontend/src/components/searchForm.tsx
@@ -6,35 +6,18 @@ import "../styles/styles.scss";
 function CourseSearchForm() {
   // Create signals for each form input
   const [courseName, setCourseName] = createSignal("");
-  const [year, setYear] = createSignal<number>(0);
   const [exam, setExam] = createSignal("");
-  const [years, setYears] = createSignal<number[]>([]);
   const [searchResults, setSearchResults] = createSignal<SearchResult[]>([]);
   const [noResultsFound, setNoResultsFound] = createSignal<boolean>(false);
-
-  async function fetchYears() {
-    try {
-      const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/year`);
-      const data: { max: number; min: number } = await response.json();
-      setYears(Array.from({ length: data.max - data.min + 1 }, (_, i) => data.min + i));
-    } catch (error) {
-      console.error("Error fetching years:", error);
-    }
-  }
-
-  onMount(() => {
-    fetchYears();
-  });
 
   // Function to handle form submission
   const handleSubmit = async (event: any) => {
     event.preventDefault(); // Prevent the default form submit action
 
-    console.log("Form submitted!", courseName(), year(), exam());
+    console.log("Form submitted!", courseName(), exam());
 
     const params = new URLSearchParams();
     if (courseName()) params.append("course", courseName());
-    if (year()) params.append("year", year().toString());
     if (exam()) params.append("exam", exam());
 
     try {
@@ -59,20 +42,11 @@ function CourseSearchForm() {
           <input id="course" value={courseName()} onInput={(e) => setCourseName(e.target.value)} />
         </div>
         <div>
-          <label for="year">Year:</label>
-          <select id="year" name="year" onInput={(e) => setYear(parseInt(e.target.value))}>
-            <option value="">Select a year</option>
-            {years().map((year) => (
-              <option value={year}>{year}</option>
-            ))}
-          </select>
-        </div>
-        <div>
           <label for="exam">Exam:</label>
           <select id="exam" value={exam()} onInput={(e) => setExam(e.target.value)}>
             <option value="">Select an exam</option>
-            <option value="Mid Sem">Mid Sem</option>
-            <option value="End Sem">End Sem</option>
+            <option value="midsem">Mid Semester</option>
+            <option value="endsem">End Semester</option>
           </select>
         </div>
         <button type="submit">Search</button>
