64ab63f80586e35a68cabd523676c1f3efd090c5
Chirag Ghosh <cghosh828049@gmail.com>
2024-02-16 22:33:36 +0530

    fix: watcher position rectified
    Signed-off-by: Chirag Ghosh <cghosh828049@gmail.com>

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,9 +1,13 @@
 /* @refresh reload */
 import { render } from "solid-js/web";
+import { inject } from "@vercel/analytics";
 
 import App from "./App";
 import "./styles/reset.scss";
+
 const root = document.getElementById("root");
+
+inject();
 
 if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
   throw new Error(
