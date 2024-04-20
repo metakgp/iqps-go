6343777a688149a0c2e887472872be5eeb18136a
Harsh Khandeparkar <shadowwarriorpro3003@gmail.com>
2024-04-21 00:08:24 +0530

    feat: simple upload landing page

--- frontend/src/pages/UploadPage.tsx
+++ frontend/src/pages/UploadPage.tsx
@@ -1,14 +1,19 @@
 import { A } from "@solidjs/router";
+import { AiOutlineCloudUpload as UploadIcon } from "solid-icons/ai";
 
 function UploadPage() {
 	return (
-		<div class="hero-screen">
+		<div class="upload-page">
 			<div class="title">
 				<h1>IQPS - Question Paper Upload</h1>
 				<p><i>Upload your question papers for future humans to use!</i></p>
 				<h3><A href="/">Question paper search</A></h3>
 			</div>
 
+			<div class="upload-area">
+				<UploadIcon size="5rem" />
+				<h2>Click or drop files to upload</h2>
+			</div>
 		</div>
 	)
 }
