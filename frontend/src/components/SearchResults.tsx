e9ae85fc1a9c8f71e70cb0fdfa7b38819d6cda4b
Harsh Khandeparkar <shadowwarriorpro3003@gmail.com>
2024-04-21 02:07:51 +0530

    feat: new icons, bigger icons

--- frontend/src/components/SearchResults.tsx
+++ frontend/src/components/SearchResults.tsx
@@ -1,8 +1,8 @@
 import { Component, For, createEffect, createSignal } from "solid-js";
 import type { SearchResult } from "../types/types";
 import { Spinner } from "./Spinner";
-import { FiDownload as DownloadIcon } from "solid-icons/fi";
-import { BiSolidCopy as ShareIcon } from "solid-icons/bi";
+import { IoLink as ShareIcon } from "solid-icons/io";
+import { FaSolidFilePdf as DownloadIcon } from "solid-icons/fa";
 import { copyLink } from "../utils/copyLink";
 
 type Props = {
@@ -133,16 +133,18 @@ const SearchResults: Component<Props> = (props) => {
                                 <a
                                   class="result-card-btn icon-btn"
                                   href={result.filelink}
+                                  title="Open PDF"
                                   target="_blank"
                                   rel="noopener noreferrer"
                                 >
-                                  <DownloadIcon />
+                                  <DownloadIcon size="1.2rem" />
                                 </a>
                                 <button
                                   class="result-card-btn icon-btn"
+                                  title="Share PDF"
                                   onClick={(e) => copyLink(e, result.filelink)}
                                 >
-                                  <ShareIcon />
+                                  <ShareIcon size="1.2rem" />
                                 </button>
                               </div>
                             </td>
