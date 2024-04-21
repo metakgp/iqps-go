5263fcebe3d9dfe12084a7cb0e3e28a471686d0d
Shikhar Soni <shikharish05@gmail.com>
2024-04-21 12:25:07 +0530

    add sql for creating courses table

--- backend/sql/init_table.sql
+++ backend/sql/init_table.sql
@@ -10,3 +10,10 @@ CREATE TABLE IF NOT EXISTS qp (
     approve_status BOOLEAN DEFAULT FALSE
 );
 SELECT setval(pg_get_serial_sequence('qp', 'id'), coalesce(max(id),0) + 1, false) FROM qp;
+
+CREATE TABLE IF NOT EXISTS courses (
+    id SERIAL PRIMARY KEY,
+    course_code TEXT NOT NULL,
+    course_name TEXT NOT NULL
+);
+COPY courses(course_code, course_name) FROM 'courses.csv' DELIMITER ',' CSV HEADER;
