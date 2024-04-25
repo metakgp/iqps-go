7cbd07caf64f86cd7dd5839c5872f66d6077cbd2
Rohan Barsagade <rbarsagade101@gmail.com>
2024-04-25 11:38:48 +0530

    feat: get course name from courses.json

--- frontend/src/utils/courseDetails.ts
+++ frontend/src/utils/courseDetails.ts
@@ -1,16 +1,24 @@
+import courses from "../data/courses.json";
+
+type Courses = {
+    [key: string]: string;
+};
+
 export const getCourseDetails = (filename: string) => {
     // filename format
-    // course_code:year:mids/ends:summer/autumn
-    const data = filename.split(":");
+    // course_code_year_midsem/endsem_summer/autumn.pdf
 
-    // TODO: API call to get course name from course code
+    const dotIndex = filename.lastIndexOf('.');
+    const [course_code, year, exam, semester] = filename.substring(0, dotIndex).split("_");
+
+    const coursesData: Courses = courses;
 
     const result = {
-        course_code: data[0] || null,
-        year: data[1] || null,
-        exam: data[2] || null,
-        semester: data[3] || null,
-        course_name: "",
+        course_code: course_code || null,
+        year: year || null,
+        exam: exam || null,
+        semester: semester.split(".")[0] || null,
+        course_name: coursesData[course_code] || null,
     };
 
     return result;
