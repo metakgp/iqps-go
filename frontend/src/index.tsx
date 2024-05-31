a8a87778475024cb7c0b6dff9a83ef25cb4183f8
Harsh Khandeparkar <shadowwarriorpro3003@gmail.com>
2024-05-31 15:52:12 +0530

    feat: initialized oauth page

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -7,6 +7,7 @@ import App from "./pages/LandingPage";
 import "./styles/styles.scss";
 
 import UploadPage from "./pages/UploadPage";
+import { OAuthPage } from "./pages/OAuth";
 
 const root = document.getElementById("root");
 
@@ -24,6 +25,7 @@ render(() => (
     <Router>
       <Route path="/" component={App} />
       <Route path="/upload" component={UploadPage} />
+      <Route path="/oauth" component={OAuthPage} />
     </Router>
     <h3 class="meta-footer">Made with ❤️ and {"</>"} by <a href="https://github.com/metakgp/iqps-go" target="_blank">MetaKGP</a></h3>
   </>
