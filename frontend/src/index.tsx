3931e295bc5aeb372b6c531d79263dd5b9c1b378
Harsh Khandeparkar <shadowwarriorpro3003@gmail.com>
2024-06-01 13:56:23 +0530

    feat: added auth provider context thingy

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -8,6 +8,7 @@ import "./styles/styles.scss";
 
 import UploadPage from "./pages/UploadPage";
 import { OAuthPage } from "./pages/OAuth";
+import { AuthProvider } from "./components/AuthProvider";
 
 const root = document.getElementById("root");
 
@@ -22,11 +23,13 @@ if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
 
 render(() => (
   <>
-    <Router>
-      <Route path="/" component={App} />
-      <Route path="/upload" component={UploadPage} />
-      <Route path="/oauth" component={OAuthPage} />
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Route path="/" component={App} />
+        <Route path="/upload" component={UploadPage} />
+        <Route path="/oauth" component={OAuthPage} />
+      </Router>
+    </AuthProvider>
     <h3 class="meta-footer">Made with ❤️ and {"</>"} by <a href="https://github.com/metakgp/iqps-go" target="_blank">MetaKGP</a></h3>
   </>
 ), root!);
