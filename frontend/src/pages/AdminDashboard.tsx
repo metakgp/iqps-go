fdd6600cda1cb30c5d186e57d0ebbdebd03a3338
Tegan Jain <teganjain527@gmail.com>
2024-06-17 15:25:52 +0530

    feat: add a navbar and a space to display the current logged in user not sure how the oauth is working so haven't created any functions

--- frontend/src/pages/AdminDashboard.tsx
+++ frontend/src/pages/AdminDashboard.tsx
@@ -1,16 +1,19 @@
-import { Component, createSignal } from "solid-js";
+import { Component } from "solid-js";
 import { PDFLister } from "../components/PDFTableHead";
 import { arr } from "../data/dummyQPs";
-import { createStore } from "solid-js/store";
-import { IAdminQuestionPaperResult } from "../types/types";
+import { A } from "@solidjs/router";
 
 export const AdminPage: Component = () => {
+  let user = "User";
   
   return (
     <div class="admin-page">
       <div class="title">
         <header>
-        <h1>IQPS - Admin Dashboard</h1>
+          <A href="/admin" class="admin">Admin Page</A>
+          <A href="/" class="search" target="_blank">Search</A>
+          <A href="/upload" class="upload" target="_blank">Upload</A>
+          <span class="user">Welcome {user}</span>
         </header>
       </div>
       <div>
