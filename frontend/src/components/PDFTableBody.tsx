3e0de4286ae20dc9d65748d98a394dc836c7d508
Harsh Khandeparkar <shadowwarriorpro3003@gmail.com>
2024-06-19 21:24:32 +0530

    feat: better pdf link styling

--- frontend/src/components/PDFTableBody.tsx
+++ frontend/src/components/PDFTableBody.tsx
@@ -38,29 +38,29 @@ export const ListElement: Component<props> = (props) => {
         <tr classList={{["qp-table-tr-pending"]: questionPaperDetails.approval === null ? true : false, ["qp-table-tr-approve"]: questionPaperDetails.approval === true, ["qp-table-tr-reject"]: questionPaperDetails.approval === false}}>
             <td><Select
                 disabled={questionPaperDetails.approval}
-                class="select"  
-                {...createOptions(Object.keys(COURSE_CODE_MAP))} 
+                class="select"
+                {...createOptions(Object.keys(COURSE_CODE_MAP))}
                 initialValue={questionPaperDetails.course_code}
                 onChange={(value) => {
-                    setQuestionPaperDetails("course_code", value);     
+                    setQuestionPaperDetails("course_code", value);
                     setQuestionPaperDetails("course_name", getCourseFromCode(value)!);
                 }}
                 />
             </td>
             <td><Select
                 disabled={questionPaperDetails.approval}
-                class="select"  
-                {...createOptions(Object.values(COURSE_CODE_MAP))} 
+                class="select"
+                {...createOptions(Object.values(COURSE_CODE_MAP))}
                 initialValue={questionPaperDetails.course_name}
                 onChange={(value) => {
-                    setQuestionPaperDetails("course_name", value); 
-                    setQuestionPaperDetails("course_code", getCodefromCourse(value));    
+                    setQuestionPaperDetails("course_name", value);
+                    setQuestionPaperDetails("course_code", getCodefromCourse(value));
                 }}
                 />
             </td>
             <td><Select
                 disabled={questionPaperDetails.approval}
-                class="select" 
+                class="select"
                 // returns every year since 1951 till today as options for year dropdown
                 {...createOptions([...Array.from({length: new Date().getFullYear() - 1950}, (e, i) => (new Date().getFullYear() - i).toString())])}
                 initialValue={questionPaperDetails.year}
@@ -89,11 +89,16 @@ export const ListElement: Component<props> = (props) => {
                 }}
                 />
             </td>
-            <td><PDFIcon/><a href={props.questionPaper.file_link} target="_blank">{fileNamer(questionPaperDetails)}</a></td>
             <td>
-                <button 
+                <div class="pdf-link">
+                    <PDFIcon size={30} />
+                    <a href={props.questionPaper.file_link} target="_blank">{fileNamer(questionPaperDetails)}</a>
+                </div>
+            </td>
+            <td>
+                <button
                 disabled={questionPaperDetails.approval}
-                classList={{["approve-button"]: questionPaperDetails.approval, ["reject-button"]: !questionPaperDetails.approval}}                    
+                classList={{["approve-button"]: questionPaperDetails.approval, ["reject-button"]: !questionPaperDetails.approval}}
                     onClick={() => {
                         paperApprove(questionPaperDetails.approval);
                     }}
