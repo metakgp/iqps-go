6e9d6b5fe866aaa06fe5e6e0ab6db310ed975841
Harsh Khandeparkar <shadowwarriorpro3003@gmail.com>
2024-08-30 12:50:28 +0530

    feat: added header to upload page

--- iqps-frontend/src/pages/UploadPage.tsx
+++ iqps-frontend/src/pages/UploadPage.tsx
@@ -1,5 +1,19 @@
+import { FaSearch } from "react-icons/fa";
+import { Header } from "../components/Common/Common";
+
 function UploadPage() {
-	return <></>;
+	return <>
+		<Header
+			title="Question Paper Upload"
+			subtitle="Upload your question papers for the benefit of humanity."
+			link={{
+				to: "/",
+				icon: FaSearch,
+				text: "Want to find a question paper?",
+				button_text: "Search!"
+			}}
+		/>
+	</>;
 }
 
 export default UploadPage;
