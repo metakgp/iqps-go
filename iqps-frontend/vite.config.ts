1894634c7649545b6c42c57949b291219a507b3b
Harsh Khandeparkar <shadowwarriorpro3003@gmail.com>
2024-09-02 21:34:38 +0530

    fix: use a better way to use pdfjs

--- iqps-frontend/vite.config.ts
+++ iqps-frontend/vite.config.ts
@@ -1,21 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import copy from 'rollup-plugin-copy';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  // Determine the correct worker source path based on the mode
-  const pdfWorkerSrc = mode === 'development' ? '/src/utils/pdf.worker.min.mjs' : '/pdf.worker.min.mjs';
-
+export default defineConfig(() => {
   return {
     plugins: [
-      react(),
-      copy({
-        targets: [
-          { src: 'src/utils/pdf.worker.min.mjs', dest: 'dist' },
-          { src: 'src/utils/pdf.worker.min.mjs', dest: 'public' }
-        ]
-      })
+      react()
     ],
     build: {
       target: 'esnext',
@@ -28,9 +18,6 @@ export default defineConfig(({ mode }) => {
           }
         }
       }
-    },
-    define: {
-      'import.meta.env.VITE_PDF_WORKER_SRC': JSON.stringify(pdfWorkerSrc)
     }
   }
 })
