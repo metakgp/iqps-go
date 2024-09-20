6c4e18dd396d1f2a89e7f6b494973c0e6d2ff4b7
Harsh Khandeparkar <shadowwarriorpro3003@gmail.com>
2024-09-20 18:01:20 +0530

    feat: bye bye solidjs

--- frontend-old-eww/src/utils/fileNamer.ts
+++
@@ -1,7 +0,0 @@
-import { IAdminQuestionPaperResult } from "../types/question_paper";
-
-export function fileNamer (qp: IAdminQuestionPaperResult) {
-    let exam = qp.exam === "endsem" ? "E" : "M";
-    let sem = qp.semester === "autumn" ? "A" : "S";
-    return `${qp.course_code}_${exam}${sem}_${qp.year}.pdf`;
-}
