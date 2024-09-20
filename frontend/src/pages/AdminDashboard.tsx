66b0a6e8730133cb3e114485ef1c31f08bea5f4c
Harsh Khandeparkar <shadowwarriorpro3003@gmail.com>
2024-09-20 18:40:21 +0530

    feat: added spinner

--- frontend/src/pages/AdminDashboard.tsx
+++ frontend/src/pages/AdminDashboard.tsx
@@ -8,20 +8,25 @@ import "./styles/admin_dashboard.scss";
 import { QPCard } from "../components/AdminDashboard/QPCard";
 import { MdLogout } from "react-icons/md";
 import PaperEditModal from "../components/Common/PaperEditModal";
+import Spinner from "../components/Spinner/Spinner";
 
 function AdminDashboard() {
 	const auth = useAuthContext();
 	const [unapprovedPapers, setUnapprovedPapers] = useState<IAdminDashboardQP[]>([]);
+	const [awaitingResponse, setAwaitingResponse] = useState<boolean>(false);
 
 	const [selectedQPaper, setSelectedQPaper] =
 		useState<IAdminDashboardQP | null>(null);
 
 	const fetchUnapprovedPapers = async () => {
+		setAwaitingResponse(true);
 		const papers = await makeRequest('unapproved', 'get', null, auth.jwt);
 
 		if (papers.status === 'success') {
 			setUnapprovedPapers(papers.data);
 		}
+
+		setAwaitingResponse(false);
 	};
 
 	useEffect(() => {
@@ -48,18 +53,23 @@ function AdminDashboard() {
 		/>
 
 		<div className="dashboard-container">
-			<p><b>Unapproved papers</b>: {unapprovedPapers.length}</p>
-			<div className="unapproved-table">
-				{unapprovedPapers.map((paper, i) => <QPCard
-					onEdit={(e) => {
-						e.preventDefault();
-						setSelectedQPaper(paper);
-					}}
-					qPaper={paper}
-					key={i}
-				/>
-				)}
-			</div>
+			{
+				awaitingResponse ? <Spinner /> :
+					<>
+						<p><b>Unapproved papers</b>: {unapprovedPapers.length}</p>
+						<div className="unapproved-table">
+							{unapprovedPapers.map((paper, i) => <QPCard
+								onEdit={(e) => {
+									e.preventDefault();
+									setSelectedQPaper(paper);
+								}}
+								qPaper={paper}
+								key={i}
+							/>
+							)}
+						</div>
+					</>
+			}
 		</div>
 
 		{selectedQPaper !== null && (
