5ccec6e043f2148b1bfa60800e5a74d4e8ac23bd
Harsh Khandeparkar <shadowwarriorpro3003@gmail.com>
2024-10-01 14:40:14 +0530

    feat: added ct in search query

--- frontend/src/components/Search/SearchForm.tsx
+++ frontend/src/components/Search/SearchForm.tsx
@@ -88,9 +88,11 @@ function CourseSearchForm() {
 				<Select
 					id="exam"
 					options={[
-						{value: '', title: 'Mid / End Semester'},
+						{value: '', title: 'Mid / End Semester / Class Test'},
+						{value: 'midend', title: 'Mid / End Semester'},
 						{value: 'midsem', title: 'Mid Semester'},
-						{value: 'endsem', title: 'End Semester'}
+						{value: 'endsem', title: 'End Semester'},
+						{value: 'ct', title: 'Class Test'}
 					]}
 					value={exam}
 					onInput={(e) => setExam(e.currentTarget.value as Exam)}
