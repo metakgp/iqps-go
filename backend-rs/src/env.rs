e84f77bac48d957bfe4a07019fae709df09c58b4
Harsh Khandeparkar <shadowwarriorpro3003@gmail.com>
2024-10-08 21:14:34 +0530

    fix,feat: many fixes, added working logs

--- backend-rs/src/env.rs
+++ backend-rs/src/env.rs
@@ -4,17 +4,6 @@ use clap::Parser;
 
 #[derive(Parser, Clone)]
 pub struct EnvVars {
-    // Paths
-    #[arg(env, default_value = "https://static.metakgp.org")]
-    /// The URL of the static files server (odin's vault)
-    pub static_files_url: String,
-    #[arg(env, default_value = "/srv/static")]
-    /// The path where static files are served from
-    pub static_file_storage_location: PathBuf,
-    #[arg(env, default_value = "iqps/uploaded")]
-    /// The path where uploaded papers are stored temporarily
-    pub uploaded_qps_path: PathBuf,
-
     // Database
     #[arg(env)]
     /// Database name
@@ -29,7 +18,6 @@ pub struct EnvVars {
     /// Database username
     pub db_user: String,
     #[arg(env)]
-
     // Auth
     /// OAuth app client id (public token)
     pub gh_client_id: String,
@@ -55,15 +43,27 @@ pub struct EnvVars {
     pub max_upload_limit: i32,
     #[arg(env, default_value = "./log/application.log")]
     /// Location where logs are stored
-    pub log_location: PathBuf
+    pub log_location: PathBuf,
+
+    // Paths
+    #[arg(env, default_value = "https://static.metakgp.org")]
+    /// The URL of the static files server (odin's vault)
+    pub static_files_url: String,
+    #[arg(env, default_value = "/srv/static")]
+    /// The path where static files are served from
+    pub static_file_storage_location: PathBuf,
+    #[arg(env, default_value = "iqps/uploaded")]
+    /// The path where uploaded papers are stored temporarily, relative to the `static_file_storage_location`
+    pub uploaded_qps_path: PathBuf,
 }
 
 impl EnvVars {
     /// Processes the environment variables after reading.
     pub fn process(mut self) -> Result<Self, Box<dyn std::error::Error>> {
-        self.static_file_storage_location = self.static_file_storage_location.canonicalize()?;
-        self.uploaded_qps_path = self.uploaded_qps_path.canonicalize()?;
-        self.log_location = self.log_location.canonicalize()?;
+        self.static_file_storage_location = std::path::absolute(self.static_file_storage_location)?;
+        self.uploaded_qps_path = std::path::absolute(self.static_file_storage_location.join(self.uploaded_qps_path))?;
+
+        self.log_location = std::path::absolute(self.log_location)?;
         Ok(self)
     }
 }
