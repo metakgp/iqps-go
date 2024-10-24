0a241cea8ef06e478121a91aeb04822d008a3d5c
Harsh Khandeparkar <shadowwarriorpro3003@gmail.com>
2024-10-24 13:56:51 +0530

    feat: working Dockerfile with portable binary build

---
+++ backend-rs/metaploy/postinstall.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+cleanup() {
+	echo "Container stopped. Removing nginx configuration."
+	rm /etc/nginx/sites-enabled/iqps.metaploy.conf
+}
+
+trap 'cleanup' SIGQUIT SIGTERM SIGHUP
+
+"${@}" &
+
+cp ./iqps.metaploy.conf /etc/nginx/sites-enabled
+
+wait $!
