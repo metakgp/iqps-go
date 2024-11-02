86baed3a54a64644d81a37a36bab044ef63a26d1
Harsh Khandeparkar <shadowwarriorpro3003@gmail.com>
2024-11-02 19:45:20 +0530

    feat: created a tag so deleting the go code

--- backend-go-old/search.sql
+++
@@ -1,4 +0,0 @@
-CREATE VIRTUAL TABLE qp_better USING fts5(course_name, tokenize="porter ascii");
-INSERT INTO qp_better SELECT course_name FROM qp;
-
-CREATE VIRTUAL TABLE qp_better2 USING fts5(course_name, tokenize='trigram');
