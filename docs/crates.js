5b2aab2a1353c09c61cd84e6c93d57252ffecb92
harshkhandeparkar <harshkhandeparkar@users.noreply.github.com>
2024-11-19 21:14:51 +0000

    docs: updated API documentation
    Signed-off-by: harshkhandeparkar <harshkhandeparkar@users.noreply.github.com>

---
+++ docs/crates.js
@@ -0,0 +1,2 @@
+window.ALL_CRATES = ["iqps_backend"];
+//{"start":21,"fragment_lengths":[14]}
---
+++ docs/trait.impl/core/convert/trait.TryFrom.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["iqps_backend",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.82.0/core/convert/trait.TryFrom.html\" title=\"trait core::convert::TryFrom\">TryFrom</a>&lt;&amp;<a class=\"struct\" href=\"https://doc.rust-lang.org/1.82.0/alloc/string/struct.String.html\" title=\"struct alloc::string::String\">String</a>&gt; for <a class=\"enum\" href=\"iqps_backend/db/queries/enum.ExamFilter.html\" title=\"enum iqps_backend::db::queries::ExamFilter\">ExamFilter</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.82.0/core/convert/trait.TryFrom.html\" title=\"trait core::convert::TryFrom\">TryFrom</a>&lt;&amp;<a class=\"struct\" href=\"https://doc.rust-lang.org/1.82.0/alloc/string/struct.String.html\" title=\"struct alloc::string::String\">String</a>&gt; for <a class=\"enum\" href=\"iqps_backend/qp/enum.Exam.html\" title=\"enum iqps_backend::qp::Exam\">Exam</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.82.0/core/convert/trait.TryFrom.html\" title=\"trait core::convert::TryFrom\">TryFrom</a>&lt;&amp;<a class=\"struct\" href=\"https://doc.rust-lang.org/1.82.0/alloc/string/struct.String.html\" title=\"struct alloc::string::String\">String</a>&gt; for <a class=\"enum\" href=\"iqps_backend/qp/enum.Semester.html\" title=\"enum iqps_backend::qp::Semester\">Semester</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[1319]}
