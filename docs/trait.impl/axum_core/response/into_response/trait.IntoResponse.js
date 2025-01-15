b9a70594e85c4ad1bfc72bdb0f17c678adb6594d
harshkhandeparkar <harshkhandeparkar@users.noreply.github.com>
2025-01-15 12:52:24 +0000

    docs: updated API documentation
    Signed-off-by: harshkhandeparkar <harshkhandeparkar@users.noreply.github.com>

---
+++ docs/trait.impl/axum_core/response/into_response/trait.IntoResponse.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["iqps_backend",[["impl IntoResponse for <a class=\"struct\" href=\"iqps_backend/routing/struct.AppError.html\" title=\"struct iqps_backend::routing::AppError\">AppError</a>"],["impl&lt;T: <a class=\"trait\" href=\"https://docs.rs/serde/1.0.210/serde/ser/trait.Serialize.html\" title=\"trait serde::ser::Serialize\">Serialize</a>&gt; IntoResponse for <a class=\"struct\" href=\"iqps_backend/routing/struct.BackendResponse.html\" title=\"struct iqps_backend::routing::BackendResponse\">BackendResponse</a>&lt;T&gt;"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[517]}
---
+++ docs/trait.impl/clap_builder/derive/trait.Parser.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["iqps_backend",[["impl Parser for <a class=\"struct\" href=\"iqps_backend/env/struct.EnvVars.html\" title=\"struct iqps_backend::env::EnvVars\">EnvVars</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[160]}
