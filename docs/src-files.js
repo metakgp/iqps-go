8d75505ebfcf0261bc53065b9800b1316f2a45ff
harshkhandeparkar <harshkhandeparkar@users.noreply.github.com>
2025-08-23 12:36:55 +0000

    docs: updated API documentation
    Signed-off-by: harshkhandeparkar <harshkhandeparkar@users.noreply.github.com>

---
+++ docs/src-files.js
@@ -0,0 +1,2 @@
+createSrcSidebar('[["import_papers",["",[],["import-papers.rs"]]],["iqps_backend",["",[["db",[],["mod.rs","models.rs","queries.rs"]],["routing",[],["handlers.rs","middleware.rs","mod.rs"]]],["auth.rs","env.rs","lib.rs","pathutils.rs","qp.rs","slack.rs"]]]]');
+//{"start":19,"fragment_lengths":[46,190]}
