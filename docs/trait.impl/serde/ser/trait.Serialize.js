f1d12ae901696291f98c8d4fdfc3f42aba49d458
harshkhandeparkar <harshkhandeparkar@users.noreply.github.com>
2025-09-06 08:26:04 +0000

    docs: updated API documentation
    Signed-off-by: harshkhandeparkar <harshkhandeparkar@users.noreply.github.com>

---
+++ docs/trait.impl/serde/ser/trait.Serialize.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["iqps_backend",[["impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.210/serde/ser/trait.Serialize.html\" title=\"trait serde::ser::Serialize\">Serialize</a> for <a class=\"enum\" href=\"iqps_backend/qp/enum.Exam.html\" title=\"enum iqps_backend::qp::Exam\">Exam</a>"],["impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.210/serde/ser/trait.Serialize.html\" title=\"trait serde::ser::Serialize\">Serialize</a> for <a class=\"enum\" href=\"iqps_backend/qp/enum.Semester.html\" title=\"enum iqps_backend::qp::Semester\">Semester</a>"],["impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.210/serde/ser/trait.Serialize.html\" title=\"trait serde::ser::Serialize\">Serialize</a> for <a class=\"struct\" href=\"iqps_backend/qp/struct.AdminDashboardQP.html\" title=\"struct iqps_backend::qp::AdminDashboardQP\">AdminDashboardQP</a>"],["impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.210/serde/ser/trait.Serialize.html\" title=\"trait serde::ser::Serialize\">Serialize</a> for <a class=\"struct\" href=\"iqps_backend/qp/struct.BaseQP.html\" title=\"struct iqps_backend::qp::BaseQP\">BaseQP</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[1120]}
